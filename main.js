0de1b5a2e6121059184511729b48bbeeee81e86b
Jacek Kirpsza <jkirpsza@gmail.com>
2020-05-06 10:25:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert('Siema :D');
+console.log('CZeść to ja Jacek');
+const name='Kamil';
+const age='23';
+console.log(name);
+console.log(age);
+console.log(`Siema jestem ${name} i mam ${age} lata.`);
+
