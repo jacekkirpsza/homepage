18e52641d41a75721d2e3033edfe651b47ec1ef4
Jacek Kirpsza <jkirpsza@gmail.com>
2020-05-08 11:10:20 +0200

    prettier added to VCS

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
-console.log('CZeść to ja Jacek');
-const name='Kamil';
-const age='23';
+console.log("CZeść to ja Jacek");
+const name = "Kamil";
+const age = "23";
 console.log(name);
 console.log(age);
 console.log(`Siema jestem ${name} i mam ${age} lata.`);
