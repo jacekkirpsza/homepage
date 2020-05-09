c5b3b1716aa16deb068fe0c4981421da223368cd
Jacek Kirpsza <jkirpsza@gmail.com>
2020-05-09 13:53:40 +0200

    BEM added to my HTML and styles chenge

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const age = "23";
 console.log(name);
 console.log(age);
 console.log(`Siema jestem ${name} i mam ${age} lata.`);
+
+const learn = document.querySelector('.learn--js');
+
+console.log(learn);
+learn.innerHTML = "Hello World, How are you ?"
+
+
