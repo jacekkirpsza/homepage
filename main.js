b7f3e03e8e99fb94b0eab4b7a7efbb4410265917
Jacek Kirpsza <jkirpsza@gmail.com>
2020-05-12 12:07:20 +0200

    JS function added i HTML and JS

--- main.js
+++ main.js
@@ -11,3 +11,49 @@ console.log(learn);
 learn.innerHTML = "Hello World, How are you ?"
 
 
+function calculate(myNumber) {
+    console.log(`Mój numer to ${myNumber}`)
+    console.log(`wynik ${myNumber*7}`);
+    return myNumber
+}
+
+const myresult = calculate(1);
+console.log(myresult);
+
+calculate(2)
+
+function greet(age, name) {
+    console.log(`mój wiek to ${age} albo ${name}`);
+    return (`Cześć jestm ${name} i mam ${age} lata`);
+}
+
+const myGreet = greet(age, name);
+console.log(myGreet);
+
+greet(2,5);
+
+function changeContent(querySelector, content) {
+    const element = document.querySelector(querySelector);
+    element.innerHTML = content;
+}
+
+const comeBack = changeContent('.learn--js', 'Hello world its me i changed you in JS');
+
+console.log(comeBack);
+
+function f(x) {
+    x = x+2;
+    console.log(x)
+    x = x*5;
+    console.log(x)
+    return x*10
+}
+f(5)
+console.log(f(1))
+
+const calculateNew = (age) => {
+    return age+3
+}
+
+console.log(calculateNew(1))
+
